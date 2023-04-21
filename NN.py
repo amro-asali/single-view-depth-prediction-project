4ef6af040b26bae2fbb7d86304b9224b9a2a1470
amro-asali <amro.asali@mail.huji.ac.il>
2023-04-21 05:24:07 +0300

    code cleanup + doc , an updated pdf

--- NN.py
+++ NN.py
@@ -1,26 +1,33 @@
+import os
 
 import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications import VGG16
-from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
-from sklearn.model_selection import train_test_split
-import numpy as np
-import os
 
 LABELS_WF = 'labels_for360_images'
-images_path = 'cars_360images'
-testFiles = 'output/test.txt'
+DATASET_IMAGES_PATH = 'cars_360images'
+TEST_FILE_NAMES = 'output/test.txt'
 
 
 class NN:
+    """
+        A neural network class that defines the architecture of the model used to predict the bounding boxes of cars
+        in images.
+
+        Attributes:
+            cnn (keras.models.Model): The compiled Keras model that represents the neural network.
+    """
     def __init__(self):
         # Create a VGG16 model with weights pre-trained on ImageNet
-        vgg = VGG16(weights="imagenet", include_top=False,input_tensor=Input(shape=(240, 240, 3)))
+        vgg = VGG16(weights="imagenet", include_top=False, input_tensor=Input(shape=(240, 240, 3)))
         vgg.trainable = False
         features = vgg.output
         features = Flatten()(features)
@@ -30,37 +37,19 @@ class NN:
         l4 = Dense(4, activation="sigmoid")(l3)
         self.cnn = Model(inputs=vgg.input, outputs=l4)
 
-def train_model():
-    # Load and preprocess data
-    data = []
-    targets = []
-    filenames = []
-    preProcess(data, filenames, targets)
-    data = np.array(data, dtype="float32") / 255.0
-    targets = np.array(targets, dtype="float32")
-
-    # Split data into training and testing sets
-    split = train_test_split(data, targets, filenames, test_size=0.1, random_state=42)
-    (trainImages, testImages) = split[:2]
-    (trainTargets, testTargets) = split[2:4]
-    (trainFilenames, testFilenames) = split[4:]
-
-    # Write test filenames to a file
-    f = open(testFiles, "w")
-    f.write("\n".join(testFilenames))
-    f.close()
-
-    # Train and save the model
-    opt = Adam(lr=1e-3)
-    model = NN().cnn
-    model.compile(loss="mse", optimizer=opt)
-    model.fit(trainImages, trainTargets,validation_data=(testImages, testTargets),epochs=25,verbose=1)
-    model.save('model', save_format="h5")
-
 
 def preprocess_row(row):
+    """
+        Preprocesses a row from the labels file and returns the corresponding image array, target values, and filename.
+
+        Args:
+            row (str): A row from the labels file in the format "filename,x_start,y_start,x_end,y_end".
+
+        Returns:
+            tuple: A tuple of the image array, target values, and filename.
+    """
     filename, x_start, y_start, x_end, y_end = row.split(",")
-    image_path = os.path.join(images_path, filename)
+    image_path = os.path.join(DATASET_IMAGES_PATH, filename)
     image = cv2.imread(image_path)
     height, width = image.shape[:2]
     x_start_norm = float(x_start) / width
@@ -71,13 +60,80 @@ def preprocess_row(row):
     array_image = img_to_array(resized_image)
     return array_image, (x_start_norm, y_start_norm, x_end_norm, y_end_norm), filename
 
-def preProcess(data, filenames, targets):
+
+def pre_process(data, filenames, targets):
+    """
+    Reads the labels file, preprocesses each row, and appends the resulting image array, target values, and filename to
+    the corresponding lists.
+
+    Args:
+        data (list): A list to which the image arrays will be appended.
+        filenames (list): A list to which the filenames will be appended.
+        targets (list): A list to which the target values will be appended.
+    """
     rows = open(LABELS_WF).read().strip().split("\n")
     for row in rows:
         array_image, target, filename = preprocess_row(row)
         data.append(array_image)
         targets.append(target)
         filenames.append(filename)
+
+
+def load_preprocessed_data():
+    """
+        Loads the preprocessed image data and target values from disk, normalizes the image data, and returns the result.
+
+        Returns:
+            tuple: A tuple of the preprocessed image data, filenames, and target values.
+    """
+    data = []
+    targets = []
+    filenames = []
+    pre_process(data, filenames, targets)
+    data = np.array(data, dtype="float32") / 255.0
+    targets = np.array(targets, dtype="float32")
+    return data, filenames, targets
+
+
+def split_data(data, filenames, targets):
+    """
+           Splits the image data, filenames, and target values into training and testing sets.
+
+           Args:
+               data (numpy.ndarray): The preprocessed image data.
+               filenames (list): The filenames corresponding to each image in the data array.
+               targets (numpy.ndarray): The target values corresponding to each image in the data array.
+
+           Returns:
+               tuple: A tuple of the testing filenames, testing image data, testing target values, training image data,
+            and training target values.
+    """
+    split = train_test_split(data, targets, filenames, test_size=0.1, random_state=42)
+    (trainImages, testImages) = split[:2]
+    (trainTargets, testTargets) = split[2:4]
+    (trainFilenames, testFilenames) = split[4:]
+    return testFilenames, testImages, testTargets, trainImages, trainTargets
+
+
+def train_model():
+    """
+       Loads the preprocessed data, splits it into training and testing sets, trains a neural network model on the training
+       data, and saves the resulting model to disk.
+    """
+    data, filenames, targets = load_preprocessed_data()
+    # Split data into training and testing sets
+    test_filenames, test_images, test_targets, train_images, train_targets = split_data(data, filenames, targets)
+    # Write test filenames to a file
+    f = open(TEST_FILE_NAMES, "w")
+    f.write("\n".join(test_filenames))
+    f.close()
+    # Train and save the model
+    opt = Adam(lr=1e-3)
+    model = NN().cnn
+    model.compile(loss="mse", optimizer=opt)
+    model.fit(train_images, train_targets, validation_data=(test_images, test_targets), epochs=25, verbose=1)
+    model.save('model', save_format="h5")
+
 
 if __name__ == '__main__':
     train_model()
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from __future__ import print_function
-
 import os
-
 import cv2
 import imutils
 import numpy as np
@@ -10,44 +8,94 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 
 
-def estimateDepth(real_world_height,focal_length,censor_height):
-    # Load the trained model
-    model = load_model('model')
+def resize_image(image_path, x_start, y_end, y_start):
+    """
+        Resizes the image at the specified path and returns relevant information.
 
-    # Process each test image
+        Parameters:
+        image_path (str): The path to the image to be resized.
+        x_start (float): The x-coordinate at which the object in the image starts.
+        y_end (float): The y-coordinate at which the object in the image ends.
+        y_start (float): The y-coordinate at which the object in the image starts.
+
+        Returns:
+        tuple: A tuple containing the height of the resized image, the resized image, the x-coordinate at which the object
+        starts, the y-coordinate at which the object ends, and the y-coordinate at which the object starts.
+    """
+    original_image = cv2.imread(image_path)
+    resized_image = imutils.resize(original_image, width=240)
+    (h, w) = resized_image.shape[:2]
+    x_start = int(x_start * w)
+    y_start = int(y_start * h)
+    y_end = int(y_end * h)
+    return h, resized_image, x_start, y_end, y_start
+
+
+def load_image(image_filename):
+    """
+       Loads the image at the specified path, resizes it, converts it to an array, and returns it along with the path to the image.
+
+       Parameters:
+       image_filename (str): The name of the image file to be loaded.
+
+       Returns:
+       tuple: A tuple containing the image array and the path to the image.
+    """
+    image_path = os.path.sep.join(['cars_me', image_filename])
+    image = load_img(image_path, target_size=(240, 240))
+    image_array = img_to_array(image) / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
+    return image_array, image_path
+
+
+def display_result(image_filename, object_depth, resized_image, x_start, y_end, y_start):
+    """
+       Displays the result of the depth estimation on the specified image.
+
+       Parameters:
+       image_filename (str): The name of the image file.
+       object_depth (float): The estimated distance to the object in the image.
+       resized_image (np.ndarray): The resized image.
+       x_start (float): The x-coordinate at which the object in the image starts.
+       y_end (float): The y-coordinate at which the object in the image ends.
+       y_start (float): The y-coordinate at which the object in the image starts.
+
+       Returns:
+       None
+    """
+    cv2.rectangle(resized_image, (x_start, y_start), (x_start, y_end), (0, 255, 0), 2)
+    depth_text = f"Distance: {object_depth:.2f} "
+    cv2.putText(resized_image, depth_text, (x_start, y_start - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.imshow("Output", resized_image)
+    cv2.imwrite(f'{image_filename}', resized_image)
+    cv2.waitKey(0)
+
+
+def estimate_depth(real_world_height, focal_length, censor_height):
+    """
+        Loads the trained object detection model and applies it on a set of test images to estimate the depth of
+        an object in the image.
+
+        Args:
+            real_world_height (float): The real world height of the object in meters.
+            focal_length (float): The focal length of the camera in millimeters.
+            censor_height (float): The height of the camera sensor in millimeters.
+
+        Returns:
+            None: The function saves the output image in the same directory as the input image with the estimated depth
+            and bounding box drawn around the detected object.
+    """
+    model = load_model('model')
     test_images = ['5.jpg', '23.jpg', '25.jpeg', '28.jpeg']
     for image_filename in test_images:
-        # Load the image and preprocess it
-        image_path = os.path.sep.join(['cars_me', image_filename])
-        image = load_img(image_path, target_size=(240, 240))
-        image_array = img_to_array(image) / 255.0
-        image_array = np.expand_dims(image_array, axis=0)
-
-        # Use the trained model to predict the bounding box coordinates
+        image_array, image_path = load_image(image_filename)
         (x_start, y_start, x_end, y_end) = model.predict(image_array)[0]
-
-        # Resize the original image and convert the bounding box coordinates to pixel values
-        original_image = cv2.imread(image_path)
-        resized_image = imutils.resize(original_image, width=240)
-        (h, w) = resized_image.shape[:2]
-        x_start = int(x_start * w)
-        y_start = int(y_start * h)
-        y_end = int(y_end * h)
-
-        # Calculate the depth of the object in the image
+        image_height_pixels, resized_image, x_start, y_end, y_start = resize_image(image_path, x_start, y_end, y_start)
         object_height_pixels = y_end - y_start
-        object_depth_cm = (h * real_world_height * focal_length) / (object_height_pixels * censor_height)
-
-        # Draw the bounding box and depth value on the image, then display it
-        cv2.rectangle(resized_image, (x_start, y_start), (x_start, y_end), (0, 255, 0), 2)
-        depth_text = f"Distance: {object_depth_cm:.2f} "
-        cv2.putText(resized_image, depth_text, (x_start, y_start - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imshow("Output", resized_image)
-        cv2.imwrite(f'{image_filename}',resized_image)
-        cv2.waitKey(0)
+        object_depth = (image_height_pixels * real_world_height * focal_length) / (object_height_pixels * censor_height)
+        display_result(image_filename, object_depth, resized_image, x_start, y_end, y_start)
 
 
 if __name__ == '__main__':
     # estimate depth for an suv with known real world height of 1.57 meters
-    estimateDepth(real_world_height=1.57,focal_length=4.1,censor_height=5.22)
+    estimate_depth(real_world_height=1.57, focal_length=4.1, censor_height=5.22)
